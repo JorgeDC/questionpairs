9c49a0ddc6d9eb05a4099acbb6bdff8c4e49571e
Jorge De Corte <jorgedecorte@gmail.com>
2018-01-09 15:16:03 +0100

    Add train script

---
+++ scripts/Train.py
@@ -0,0 +1,190 @@
+from time import time
+import pandas as pd
+import numpy as np
+from gensim.models import KeyedVectors
+import re
+from nltk.corpus import stopwords
+import nltk
+from sklearn.model_selection import train_test_split
+
+import itertools
+import datetime
+
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Bidirectional
+from keras.models import Model
+from keras.layers import Input, Embedding, LSTM, Merge
+from keras import backend as K
+from keras.optimizers import Adadelta
+from keras.callbacks import ModelCheckpoint
+
+TRAIN_CSV = "../dutch_data/dutch_formatted_as_csv.csv"
+EMBEDDING_FILE = '../wiki.nl/wiki.nl.vec'
+train_df = pd.read_csv(TRAIN_CSV)
+
+def substitute_thousands(text):
+    matches = re.finditer(r'[0-9]+(?P<thousands>\s{0,2}k\b)', text, flags=re.I)
+    result = ''
+    len_offset = 0
+    for match in matches:
+        result += '{}000'.format(text[len(result)-len_offset:match.start('thousands')])
+        len_offset += 3 - (match.end('thousands') - match.start('thousands'))
+    result += text[len(result)-len_offset:]
+    return result
+
+
+nltk.download('stopwords')
+print("begin sets")
+stops = set(stopwords.words('dutch'))
+print("end sets")
+
+def text_to_word_list(text):
+    ''' Pre process and convert texts to a list of words '''
+    text = str(text)
+    text = text.lower()
+
+    # Clean the text
+    text = substitute_thousands(text)
+    text = re.sub(r",", " ", text)
+    text = re.sub(r"\.", " ", text)
+    text = re.sub(r"!", " ! ", text)
+    text = re.sub(r"\/", " ", text)
+    text = re.sub(r"\^", " ^ ", text)
+    text = re.sub(r"\+", " + ", text)
+    text = re.sub(r"\-", " - ", text)
+    text = re.sub(r"\=", " = ", text)
+    text = re.sub(r"'", " ", text)
+    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+    text = re.sub(r":", " : ", text)
+    text = re.sub(r"\0s", "0", text)
+    text = re.sub(r" 9 11 ", "911", text)
+    text = re.sub(r"e - mail", "email", text)
+
+    text = text.split()
+
+    return text
+
+
+vocabulary = dict()
+inverse_vocabulary = ['<unk>']
+# '<unk>' will never be used, it is only a placeholder for the
+# [0, 0, ....0] embedding
+
+print("loading word2vec")
+word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, binary=False)
+print("done loading word2vec")
+
+questions_cols = ['question1', 'question2']
+
+# Iterate over the questions only of both training and test datasets
+for dataset in [train_df]:
+    for index, row in dataset.iterrows():
+
+        # Iterate through the text of both questions of the row
+        for question in questions_cols:
+
+            q2n = []  # q2n -> question numbers representation
+            for word in text_to_word_list(row[question]):
+
+                # Check for unwanted words
+                if word in stops and word not in word2vec.vocab:
+                    continue
+
+                if word not in vocabulary:
+                    vocabulary[word] = len(inverse_vocabulary)
+                    q2n.append(len(inverse_vocabulary))
+                    inverse_vocabulary.append(word)
+                else:
+                    q2n.append(vocabulary[word])
+
+            # Replace questions with lists of word indices
+            dataset.set_value(index, question, q2n)
+
+embedding_dim = 300
+# This will be the embedding matrix
+embeddings = 1 * np.random.randn(len(vocabulary) + 1, embedding_dim)
+embeddings[0] = 0  # So that the padding will be ignored
+
+# Build the embedding matrix
+for word, index in vocabulary.items():
+    if word in word2vec.vocab:
+        embeddings[index] = word2vec.word_vec(word)
+
+
+max_seq_length = max(train_df.question1.map(lambda x: len(x)).max(),train_df.question2.map(lambda x: len(x)).max())
+
+# Split to train validation
+validation_size = 20000
+training_size = len(train_df) - validation_size
+
+X = train_df[questions_cols]
+Y = train_df['is_duplicate']
+
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, Y, test_size=validation_size)
+
+# Split to dicts
+X_train = {'left': X_train.question1, 'right': X_train.question2}
+X_validation = {'left': X_validation.question1, 'right': X_validation.question2}
+
+# Convert labels to their numpy representations
+Y_train = Y_train.values
+Y_validation = Y_validation.values
+
+# Zero padding
+for dataset, side in itertools.product([X_train, X_validation], ['left', 'right']):
+    dataset[side] = pad_sequences(dataset[side], maxlen=max_seq_length)
+
+# Make sure everything is ok
+assert X_train['left'].shape == X_train['right'].shape
+assert len(X_train['left']) == len(Y_train)
+
+# Model variables
+n_hidden = 50
+gradient_clipping_norm = 1.25
+batch_size = 64
+n_epoch = 11
+
+def exponent_neg_manhattan_distance(left, right):
+    ''' Helper function for the similarity estimate of the LSTMs outputs'''
+    return K.exp(-K.sum(K.abs(left-right), axis=1, keepdims=True))
+
+# The visible layer
+left_input = Input(shape=(max_seq_length,), dtype='int32')
+right_input = Input(shape=(max_seq_length,), dtype='int32')
+
+embedding_layer = Embedding(len(embeddings), embedding_dim, weights=[embeddings], input_length=max_seq_length, trainable=False)
+
+# Embedded version of the inputs
+encoded_left = embedding_layer(left_input)
+encoded_right = embedding_layer(right_input)
+
+# Since this is a siamese network, both sides share the same LSTM
+shared_lstm = LSTM(n_hidden)
+
+left_output = shared_lstm(encoded_left)
+right_output = shared_lstm(encoded_right)
+
+# Calculates the distance as defined by the MaLSTM model
+malstm_distance = Merge(mode=lambda x: exponent_neg_manhattan_distance(x[0], x[1]), output_shape=lambda x: (x[0][0], 1))([left_output, right_output])
+
+# Pack it all up into a model
+malstm = Model([left_input, right_input], [malstm_distance])
+
+# Adadelta optimizer, with gradient clipping by norm
+optimizer = Adadelta(clipnorm=gradient_clipping_norm)
+
+# checkpoint
+filepath="weights-improvement-{epoch:02d}-{val_acc:.2f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+callbacks_list = [checkpoint]
+
+malstm.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['accuracy'])
+
+
+# Start training
+training_start_time = time()
+
+malstm_trained = malstm.fit([X_train['left'], X_train['right']], Y_train, batch_size=batch_size, callbacks=callbacks_list, epochs=n_epoch,
+                            validation_data=([X_validation['left'], X_validation['right']], Y_validation))
+
+print("Training time finished.\n{} epochs in {}".format(n_epoch, datetime.timedelta(seconds=time()-training_start_time)))
